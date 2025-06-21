e364c680f4c3005b3b3eff0a5efcb179f8781b94
makoto <akagami8130@gmail.com>
2025-06-21 19:25:28 +0900

    お問い合わせフォームの雛形作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('index');
 });
+
+//お問い合わせフォーム
+Route::get("/contact", [ContactController::class, "index"])->name("contact");
+Route::post("/contact", [ContactController::class, "sendMail"]);
+Route::get("/contact/complete", [ContactController::class, "complete"])->name("contact.complete");
